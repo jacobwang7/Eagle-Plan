4dbc013d61a58ff2dba81deb22c6ca4180736f32
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-01-22 23:13:14 -0500

    Added some user input features.
    Still have to work out the loop to keep selecting courses and another
    user input scanner to decide which semester to place course in.
    
    Considering making each Eagle_plan object a semester rather than all 8
    semesters.

--- Eagle_plan/src/eagleplan/Eagle_plan.java
+++ Eagle_plan/src/eagleplan/Eagle_plan.java
@@ -10,9 +10,24 @@ public class Eagle_plan {
 	
 	private ArrayList<Course> semester_1;
 	private ArrayList<Course> semester_2;
+	private ArrayList<Course> semester_3;
+	private ArrayList<Course> semester_4;
+	private ArrayList<Course> semester_5;
+	private ArrayList<Course> semester_6;
+	private ArrayList<Course> semester_7;
+	private ArrayList<Course> semester_8;
+	
 	public Eagle_plan() {
+		
 		semester_1 = new ArrayList<Course>();
 		semester_2 = new ArrayList<Course>();
+		semester_3 = new ArrayList<Course>();
+		semester_4 = new ArrayList<Course>();
+		semester_5 = new ArrayList<Course>();
+		semester_6 = new ArrayList<Course>();
+		semester_7 = new ArrayList<Course>();
+		semester_8 = new ArrayList<Course>();
+		
 	}
 
 	
@@ -35,18 +50,47 @@ public static Course getComporg() {
 /*public static void setCourse(String newCourse) {
 	this.Course = newCourse;
 }*/
+
+
+public Course selector() {
+	Scanner ask_course = new Scanner(System.in);  // Create a Scanner object
+	System.out.println("Choose from these courses: ");
+	for (Course i: Course.values()) {
+		System.out.println(i);
+	}
+	
+	while(true) {
+		try {
+			String c = ask_course.next();
+			System.out.println("Selected course: " + Course.valueOf(c));
+			Course c1 = Course.valueOf(c);
+			return c1;
+		}
+		catch (IllegalArgumentException error) {
+			System.out.println("  Error: " + error);
+		}
+	}
+}
+
+
 	public static void main(String[] args) {
 		Eagle_plan plan1 = new Eagle_plan();
-		Course class1 = getCS1();
+		System.out.println(plan1.selector());
+			
+		
+		/*Course class1 = getCS1();
 		Course class2 = getCS2();
 		plan1.semester_1.add(class1);
 		plan1.semester_2.add(class2);
 		System.out.println(plan1.semester_1);
-		System.out.println(plan1.semester_2);
+		System.out.println(plan1.semester_2);*/
+		
 		
 		// TODO Auto-generated method stub
 		
 		
+		//USE SWITCH CASE BUT c = User input
+		//see if user input can input enum
 		
 		/*Course c = Course.CS2;
 		
